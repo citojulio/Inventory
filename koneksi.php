c068604daebda0888b3b96c3dbd336f09f2f3d59
citojulio <164726818+citojulio@users.noreply.github.com>
2025-11-20 19:26:09 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,23 @@
 <?php
-$host = "127.0.0.1"; // <--- UBAH DARI localhost JADI INI
-$user = "root";
-$pass = "";
-$db   = "db_inventory";
-$port = "9306"; // Ganti dengan port yang kamu lihat di XAMPP
-$conn = mysqli_connect($host, $user, $pass, $db, $port); // Tambahkan $port di akhir
+$host = "web-tugas-cito-server.mysql.database.azure.com";
+$user = "swupczrzan@web-tugas-cito-server";
+$pass = "Permisi123";
+$db   = "web-tugas-cito-database";
+
+$conn = mysqli_init();
+
+mysqli_real_connect(
+    $conn,
+    $host,
+    $user,
+    $pass,
+    $db,
+    3306,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
 
 if (!$conn) {
-    die("Koneksi Gagal: " . mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
-
 ?>
-
